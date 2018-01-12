015dbf1dfab98fdfe977cbb8ec8023ec1281a90c
Matthew Turner <mturner737@gmail.com>
2018-01-11 22:25:55 -0500

    Organizational changes.

--- Real_Estate_Analysis.py
+++ Real_Estate_Analysis.py
@@ -3,7 +3,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from utilities.utility import HTMLTableToDF, HTMLtoDF, URLDetails
 
-url = 'https://salesweb.civilview.com/Sales/SalesSearch?countyId=9'
+url = 'https://salesweb.civilview.com/Sales/SalesSearch?countyId=8'
 detailed_url = 'https://salesweb.civilview.com/Sales/SaleDetails?PropertyId=361762545'
 baseUrl = 'https://salesweb.civilview.com'
 
@@ -11,18 +11,6 @@ baseUrl = 'https://salesweb.civilview.com'
 
 RealEstateData = pd.DataFrame()
 CountyURLs = {}
-listingDetails = ['Sales Date :', 'Sheriff # :', 'Priors :', 'Defendant :', 'Plaintiff :', 'Court Case # :', \
-                  'Address :', 'Approx. Judgment* :', 'Attorney :', 'Description :', 'Approx. Upset* :']
-statusTypes = ['Pending Bankruptcy', 'Adjourned - Defendant', 'Adjourned - Court', 'Scheduled', \
-               'Adjourned - Plaintiff', 'Cancelled/Settled', 'Adjourned - Other', 'Purchased - 3rd Party', \
-               'Purchased - Plaintiff', 'Cancelled', 'Indefinite Bankruptcy', 'Vacated', 'Canceled', \
-               'Adjournment Plaintiff', 'Adjournment Defendant', 'Purchased - Buy Back', 'Purchased - Third Party', \
-               'Settled', 'Bankrupt', 'Reinstated', 'Redeemed', 'Vacate ', 'ADJOURNED DUE TO BANKRUPTCY', \
-               'Defendant Adjournment', 'Plaintiff Adjournment', 'Bankruptcy', 'Bankuptcy', \
-               'Adjourned per Court Order', 'Buy Back', 'Purchased', 'Sheriff Adjournment', 'On Hold', \
-               'Hold In Abeyance', 'Adjourned - Bankruptcy', 'Adjourned - Statuatory', 'Rescheduled', 'Re-Scheduled']
-purchaseTypes = ['Purchased - 3rd Party', 'Purchased - Plaintiff', 'Purchased - Buy Back', 'Purchased - Third Party', \
-                 'Purchased']
 
 ##
 
@@ -34,6 +22,4 @@ for county in RealEstateSoup.findAll('a'):
 
 ##
 
-HTMLtoDF(url).to_excel('C:\\Users\\matth\\Desktop\\Real Estate Analysis Ouput.xlsx','Sheet1')
-
-RealEstateData = pd.DataFrame()
+HTMLtoDF(url)
